d153ac94d87e34bde894adef23c31d82836ac331
anthelmekoffi <anthelmekoffi77@gmail.com>
2025-07-16 16:58:45 +0000

    config projet

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 ### acl_package
 Package de gestion des ACL avec une base de données SQLAchemy et MongoDB
+
+
+### ALEMBIC POUR LA GESTION DES MIGRATIONS AVEC SQLALCHEMY
+
+```
+pip install alembic
+```
+#### Exécuter pour générer le dossier de migration et alembic.ini
+```
+alembic init alembic
+```
+
---
+++ app/core/common/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from typing import AsyncGenerator
+from app.core.common.setting import reload_settings
+
+settings = reload_settings()
+class Database:
+    def __init__(self):
+        self.engine = create_async_engine(settings.DATABASE_URL, echo=False, future=True)
+        self.AsyncSessionLocal = sessionmaker(
+            bind=self.engine,
+            class_=AsyncSession,
+            expire_on_commit=False
+        )
+        self.Base = declarative_base()
+
+    async def get_db(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.AsyncSessionLocal() as session:
+            yield session
